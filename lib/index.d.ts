6016e199947b441f9d3ac8ff0a60104ebd8b8f45
Ding Yubo <dingyubo@4paradigm.com>
2020-02-22 14:14:12 +0800

    update d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -74,4 +74,46 @@ declare class VueFrame extends React.Component<IProps, {}> {
 	render(): JSX.Element;
 }
 
+declare class ReactFrame extends React.Component<IProps, {}> {
+	private loadType: IProps['loadType'];
+  private currentName: string;
+  private visible: boolean;
+  private currentUrl: string;
+  private currentCSSUrl: string;
+  private currentPublicPath: string;
+  private publicPathKey: string;
+  private publicPathReg: RegExp;
+  private rootNodeWrapper: React.RefObject<HTMLDivElement>;;
+  private component: any;
+  private parcel: any;
+  private vueWrapper1: HTMLDivElement;
+  private vueWrapper2: HTMLDivElement;
+  private styleElements: HTMLLinkElement[] | HTMLStyleElement[];
+
+	constructor(props: IProps);
+	componentDidMount(): void;
+	componentDidUpdate(prevProps: IProps): void;
+	componentWillUnmount(): void;
+	private initHack(): void;
+	private initHackSelector(name: keyof ISelecotr, originSelectorFn: ((id: string) => HTMLElement | null) |
+	(<E extends Element = Element>(id: string) => NodeListOf<E>) |
+	HTMLCollectionOf<Element>): void;
+	private internalHackSelector(selectorMap: { [name: string]: Function },name: keyof ISelecotr,
+    codeIsExecuting: boolean,): void;
+	private internalHackCSSsandbox(originAppendChild: <T extends Node>(this: any, newChild: T) => T,
+	codeIsExecuting: boolean,): void;
+	private registerComponentAndMount(component: object): void;
+	private addComponentToPage(rootEleWrapper: HTMLDivElement): void;
+	private registerVueComponent(el: string | HTMLElement, vueComponent: object, id: string): object;
+	private isVueComponent(component: any): boolean;
+	private getOriginCode(url: string, method: string, data?: any): Promise<any>;
+	private getCurrentName(self: any): string;
+	private executeOriginCode(code: string): Self;
+	private getOriginVueComponent(): object;
+
+	render(): JSX.Element;
+}
+
 export default VueFrame;
+
+export { ReactFrame, VueFrame }
